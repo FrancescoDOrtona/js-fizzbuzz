c49c219d537d849089930dc5a6fd1c42cc2c25bb
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-06 15:46:27 +0200

    Fix: if order

--- js/app.js
+++ js/app.js
@@ -1,21 +1,19 @@
 //  Creo un loop con con For che stampi i numeri da 1 a 100 nella console
 
-for(let i = 0; i < 100; i++) {
-    let n = i + 1
-    
+for (let i = 0; i < 100; i++) {
+  let n = i + 1;
 
-    const restoDiv3 = n % 3
-    const restoDiv5 = n % 5
+  // 15
+  const restoDiv3 = n % 3; // 0
+  const restoDiv5 = n % 5; // 0
 
-    if (restoDiv3 === 0){
-        console.log(n,'Fizz')
-    }
-    if (restoDiv5 === 0){
-        console.log(n,'Buzz')
-    }
-    if (restoDiv3 === 0 && restoDiv5 === 0) {
-        console.log(n,'FizzBuzz')
-    }
-
-
+  if (restoDiv3 === 0 && restoDiv5 === 0) { // SI && SI
+    console.log(n, "FizzBuzz");
+  } else if (restoDiv3 === 0) {
+    // (0 === 0) SI
+    console.log(n, "Fizz");
+  } else if (restoDiv5 === 0) {
+    // (0 === 0)
+    console.log(n, "Buzz");
+  }
 }
